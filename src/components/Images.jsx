184725fce82fe60b9e9b03b77235bcc4869e732d
Matayay Karuna <matayay2@gmail.com>
2023-02-22 22:04:57 -0700

    added images and title

---
+++ src/components/Images.jsx
@@ -0,0 +1,13 @@
+import darkhorse from '../assets/darkhorse.jpeg';
+import snarf from '../assets/snarf.jpeg';
+import buff from '../assets/thebuff.jpeg';
+import sforno from '../assets/sforno.jpeg';
+import tea from '../assets/tea.jpeg';
+import avanti from '../assets/avanti.jpeg';
+import french from'../assets/french.jpeg';
+import tibet from '../assets/tibet.jpeg'
+import sink from '../assets/sink.jpeg';
+import pete from '../assets/pete.jpeg';
+
+const restaurants = [darkhorse, snarf, buff, sforno, tea, avanti, french, tibet, sink, pete];
+export default restaurants;
---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = () => {
+    return (
+        <div className="Title">
+            <h1>Popular Restaurants in Boulder, CO</h1>
+        </div>
+    )
+};
+
+export default Title;
